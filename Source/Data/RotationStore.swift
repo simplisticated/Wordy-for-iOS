7b35bc0b0380990bc0b175db2da8504eea76c26e
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 21:29:32 +0300

    Added RotationStore class

---
+++ Source/Data/RotationStore.swift
@@ -0,0 +1,38 @@
+//
+//  RotationStore.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+internal class RotationStore {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init() {
+        self.rules = []
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    private var rules: [TextRotationRule]!
+    
+    // MARK: Public object methods
+    
+    // MARK: Private object methods
+    
+}
