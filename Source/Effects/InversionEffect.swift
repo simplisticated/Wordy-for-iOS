3dd958e3dde40cfb6bc84bdf44117b8eacb5a94d
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 23:24:29 +0300

    Added InversionEffect class

---
+++ Source/Effects/InversionEffect.swift
@@ -0,0 +1,42 @@
+//
+//  InversionEffect.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public class InversionEffect {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init() {
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    // MARK: Private object methods
+    
+}
+
+extension InversionEffect: Effect {
+    
+    public func filteredText(from sourceText: String) -> String {
+        return ""
+    }
+}
