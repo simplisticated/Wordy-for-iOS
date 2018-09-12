5c33efecfb75fad56ffe725c64df0e3ee4bad06d
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 21:19:26 +0300

    Added TextRotation enumeration

---
+++ Source/Types/TextRotation.swift
@@ -0,0 +1,15 @@
+//
+//  TextRotation.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum TextRotation {
+    case normal
+    case upsideDown
+    case inverted
+}
