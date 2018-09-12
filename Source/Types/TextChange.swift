88a58b75ead98dbb32904593de9f49587fe5177a
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 21:24:18 +0300

    Added TextChange structure

---
+++ Source/Types/TextChange.swift
@@ -0,0 +1,14 @@
+//
+//  TextChange.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+internal struct TextChange {
+    public var effect: Effect
+    public var segment: TextSegment
+}
