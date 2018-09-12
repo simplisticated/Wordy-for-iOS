84be56534d0a3f2c22d02b3196b1675c4cb41967
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 21:11:52 +0300

    Added TextFilter class

---
+++ Source/TextFilter.swift
@@ -0,0 +1,38 @@
+//
+//  TextFilter.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public class TextFilter {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init(sourceText: String) {
+        self.sourceText = sourceText
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    fileprivate var sourceText: String
+    
+    // MARK: Public object methods
+    
+    // MARK: Private object methods
+    
+}
