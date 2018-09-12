e29cc5a3ceb001b0d54e6fa43f7527590eff821e
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 21:19:12 +0300

    Added TextCase enumeration

---
+++ Source/Types/TextCase.swift
@@ -0,0 +1,16 @@
+//
+//  TextCase.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum TextCase {
+    case allUpper
+    case allLower
+    case firstUpperNextLower
+    case firstLowerNextUpper
+}
