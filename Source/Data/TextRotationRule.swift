2d0d771f244a0cf6a452d74ac6f2907fd025e4db
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 21:27:05 +0300

    Added TextRotationRule structure

---
+++ Source/Data/TextRotationRule.swift
@@ -0,0 +1,14 @@
+//
+//  TextRotationRule.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+internal struct TextRotationRule {
+    public var normal: String
+    public var rotated: String
+}
