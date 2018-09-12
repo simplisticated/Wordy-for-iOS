6b7c6059071368a6b3589d0957bc39f130a79f3b
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 23:22:58 +0300

    Implemented RotationEffect

---
+++ Source/Effects/RotationEffect.swift
@@ -0,0 +1,100 @@
+//
+//  RotationEffect.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public class RotationEffect {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init(rotation: TextRotation, caseSensitive: Bool = true) {
+        self.rotation = rotation
+        self.caseSensitive = caseSensitive
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    fileprivate var rotation: TextRotation
+    
+    fileprivate var caseSensitive: Bool
+    
+    // MARK: Public object methods
+    
+    // MARK: Private object methods
+    
+}
+
+extension RotationEffect: Effect {
+    
+    public func filteredText(from sourceText: String) -> String {
+        var filteredText = ""
+        let textLength = sourceText.count
+        
+        let rotationStore = RotationStore()
+        let ruleCount = rotationStore.ruleCount
+        
+        for letterIndex in 0..<textLength {
+            let sourceLetterStringIndex = sourceText.index(sourceText.startIndex, offsetBy: letterIndex)
+            let sourceLetter = String(sourceText[sourceLetterStringIndex...sourceLetterStringIndex])
+            let isUppercase = CharacterSet.uppercaseLetters.contains(UnicodeScalar(sourceLetter)!)
+            var filteredLetter = String(sourceLetter)
+            
+            for ruleIndex in 0..<ruleCount {
+                let rule = rotationStore.rule(forIndex: ruleIndex)
+                let isNormal = sourceLetter.lowercased() == rule.normal.lowercased()
+                let isRotated = sourceLetter.lowercased() == rule.rotated.lowercased()
+                
+                switch self.rotation {
+                case .normal:
+                    if isRotated {
+                        filteredLetter = String(rule.normal)
+                    }
+                    break
+                case .upsideDown:
+                    if isNormal {
+                        filteredLetter = String(rule.rotated)
+                    }
+                    break
+                case .inverted:
+                    if isNormal {
+                        filteredLetter = rule.rotated
+                    } else if isRotated {
+                        filteredLetter = rule.normal
+                    }
+                    break
+                }
+                
+                let foundAppropriateRule = isNormal || isRotated
+                
+                if foundAppropriateRule {
+                    break
+                }
+            }
+            
+            if self.caseSensitive {
+                filteredLetter = isUppercase ? filteredLetter.uppercased() : filteredLetter.lowercased()
+            }
+            
+            filteredText += filteredLetter
+        }
+        
+        return filteredText
+    }
+    
+}
