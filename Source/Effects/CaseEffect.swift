7c9e934b54cc288b8bbf204f536fbe362c6d162d
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-12 22:09:01 +0300

    Implemented CaseEffect

---
+++ Source/Effects/CaseEffect.swift
@@ -0,0 +1,89 @@
+//
+//  CaseEffect.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public class CaseEffect {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init(textCase: TextCase) {
+        self.textCase = textCase
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    fileprivate var textCase: TextCase
+    
+    // MARK: Public object methods
+    
+    // MARK: Private object methods
+    
+}
+
+extension CaseEffect: Effect {
+    
+    public func filteredText(from sourceText: String) -> String {
+        switch self.textCase {
+        case .allUpper:
+            return sourceText.uppercased()
+        case .allLower:
+            return sourceText.lowercased()
+        case .firstUpperNextLower:
+            if sourceText.isEmpty {
+                return ""
+            } else {
+                let firstLetterStartIndex = sourceText.startIndex
+                let firstLetterLastIndex = sourceText.startIndex
+                let firstLetter = String(sourceText[firstLetterStartIndex...firstLetterLastIndex])
+                
+                var otherText = ""
+                
+                if sourceText.count > 1 {
+                    let otherTextStartIndex = sourceText.index(sourceText.startIndex, offsetBy: 1)
+                    let otherTextEndIndex = sourceText.endIndex
+                    otherText = String(sourceText[otherTextStartIndex...otherTextEndIndex])
+                }
+                
+                return firstLetter.uppercased()
+                    + otherText.lowercased()
+            }
+        case .firstLowerNextUpper:
+            if sourceText.isEmpty {
+                return ""
+            } else {
+                let firstLetterStartIndex = sourceText.startIndex
+                let firstLetterLastIndex = sourceText.startIndex
+                let firstLetter = String(sourceText[firstLetterStartIndex...firstLetterLastIndex])
+                
+                var otherText = ""
+                
+                if sourceText.count > 1 {
+                    let otherTextStartIndex = sourceText.index(sourceText.startIndex, offsetBy: 1)
+                    let otherTextEndIndex = sourceText.endIndex
+                    otherText = String(sourceText[otherTextStartIndex...otherTextEndIndex])
+                }
+                
+                return firstLetter.lowercased()
+                    + otherText.uppercased()
+            }
+        }
+    }
+    
+}
