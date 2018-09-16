dbae99977cd9e49782ff46652cd3936813348bdb
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-16 22:34:02 +0300

    Renamed TextSegment to SubstringLocation

---
+++ Source/Types/SubstringLocation.swift
@@ -0,0 +1,14 @@
+//
+//  SubstringLocation.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 12.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+internal struct SubstringLocation {
+    public var startIndex: Int
+    public var endIndex: Int
+}
