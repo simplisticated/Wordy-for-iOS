a6ddd359a36c895c4e332494a6130823ae333b92
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-17 17:16:28 +0300

    Added Plugin class

---
+++ Source/Customization/Plugin.swift
@@ -0,0 +1,42 @@
+//
+//  Plugin.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 17.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+open class Plugin {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init(sourceText: String) {
+        self.sourceText = sourceText
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    public fileprivate(set) var sourceText: String
+    
+    // MARK: Public object methods
+    
+    public func result() -> String {
+        return ""
+    }
+    
+    // MARK: Private object methods
+    
+}
