b30dc73783e6e021ca36a872fe5033dceb2b8e5b
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-17 15:38:22 +0300

    Added Language enumeration

---
+++ Source/Types/Language.swift
@@ -0,0 +1,14 @@
+//
+//  Language.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 17.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum Language {
+    case english
+    case russian
+}
