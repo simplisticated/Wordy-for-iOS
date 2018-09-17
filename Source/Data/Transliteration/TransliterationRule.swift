2b6abcbf847d76c55b510e2db2f213ebdd81a31c
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-09-17 15:53:20 +0300

    Added TransliterationRule structure

---
+++ Source/Data/Transliteration/TransliterationRule.swift
@@ -0,0 +1,14 @@
+//
+//  TransliterationRule.swift
+//  Wordy
+//
+//  Created by Igor Matyushkin on 17.09.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+internal struct TransliterationRule {
+    public var replaceable: String
+    public var replacement: String
+}
